dd7b27caf8f423f006224ef928c97075a0445444
Gotti0 <dlgusdn946@gmail.com>
2025-06-08 10:34:27 +0900

    trans_llm_api:translate_single_query_thread_safe:867 - Error translating query '若い娘の中に・...': local variable 'current_time' referenced before assignment. Attempt

--- modules/translators/trans_llm_api.py
+++ modules/translators/trans_llm_api.py
@@ -623,8 +623,8 @@ class GeminiTranslator(BaseTranslator):
         # _respect_key_limit correctly handles the single key case.
 
         time_since_last_request = current_time - self.last_request_time
-        current_time = time.time() # current_time 정의
-        
+        current_time = time.time() # Define current_time here
+
         delay = float(self.get_param_value("delay"))
         if time_since_last_request < delay:
             sleep_time = delay - time_since_last_request
